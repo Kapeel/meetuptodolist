7bcf6933fb48e84919d95b526c740e840d2f24b2
luismenjivar <luismenjivr@gmail.com>
2015-09-16 10:16:35 -0400

    added basic angular

--- public/angular/todoscontroller.js
+++ public/angular/todoscontroller.js
@@ -1,1 +1,6 @@
-console.log("hello world from the todoscontroller.js file to the console")
+// console.log("hello world from the todoscontroller.js file to the console")
+var todoApp = angular.module('todoApp', []);
+
+todoApp.controller('todosController', ['$scope', '$http', function ($scope, $http){
+  $scope.name = ""
+}])
